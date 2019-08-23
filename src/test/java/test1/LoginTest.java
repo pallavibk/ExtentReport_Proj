daf8b221270761af447eae933f6e741f00179c4b
pallavibk <pallavibk@github.com>
2019-08-23 22:13:52 +0530

    F

---
+++ src/test/java/test1/LoginTest.java
@@ -0,0 +1,32 @@
+package test1;
+
+
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	// public static WebDriver driver;
+
+	WebDriver driver;
+	
+
+	@Test
+
+	public void f2() throws InterruptedException {
+	System.out.println("first test");
+	}
+	@Test
+
+	public void f3() throws InterruptedException {
+
+	
+	System.out.println("second test");
+	}
+
+	
+}
